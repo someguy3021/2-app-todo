c1131ab881539b696a0691a597e1aa8d7059242d
someguy3021 <162592428+someguy3021@users.noreply.github.com>
2024-04-26 18:56:12 +0500

    Task 10.2

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,82 @@
 <template>
-  MyTodoList
+  <div class="wrapper-todo">
+    <div class="title has-text-centered">My ToDo list</div>
+    <form @submit.prevent="addTodo">
+      <div class="field is-grouped mb-5">
+        <p class="control is-expanded">
+          <input
+            v-model="newtodoContent"
+            class="input"
+            type="text"
+            placeholder="Add a todo"
+          />
+        </p>
+        <p class="control">
+          <button :disabled="!newtodoContent" class="button is-info">
+            Add
+          </button>
+        </p>
+      </div>
+    </form>
+    <div v-for="todo in todos" :key="todo.id" class="card mb-5">
+      <div class="card-content">
+        <div class="content">
+          <div class="columns is-mobile is-vcentered">
+            <div class="column">{{ todo.content }}</div>
+            <div class="column is-5 has-text-right">
+              <button class="button is-light">&check;</button>
+              <button class="button is-danger ml-2">&cross;</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup>
+// import
+import { ref } from "vue";
+import { v4 as uuidv4 } from 'uuid';
+
+// todos
+
+const todos = ref([
+  // {
+  //   id: "id1",
+  //   content: "hello",
+  //   done: false,
+  // },
+  // {
+  //   id: "id2",
+  //   content: "check this",
+  //   done: false,
+  // },
+]);
+
+//add todo
+
+const newtodoContent = ref("");
+
+const addTodo = () => {
+  const newTodo = {
+    id: uuidv4(),
+    content: newtodoContent.value,
+    done: false,
+  };
+  todos.value.unshift(newTodo);
+  newtodoContent.value ="";
+};
+</script>
+
+
+<style>
+@import "bulma/css/bulma.min.css";
+
+.wrapper-todo {
+  max-width: 60%;
+  min-width: 400px;
+  padding: 20px;
+  margin: 0px auto;
+}
+</style>
